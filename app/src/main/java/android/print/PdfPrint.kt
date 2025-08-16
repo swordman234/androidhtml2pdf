1838ad9b6f082f06de926e4f6219c711780b8e9b
Arya Wibisono <swordman234>
2025-08-16 18:19:07 +0700

    remove redundant class in pdfPrint

--- app/src/main/java/android/print/PdfPrint.kt
+++ app/src/main/java/android/print/PdfPrint.kt
@@ -1,9 +1,6 @@
 package android.print
 
-import android.os.CancellationSignal
 import android.os.ParcelFileDescriptor
-import android.util.Log
-import java.io.File
 
 class PdfPrint(private val printAttributes: PrintAttributes) {
 
@@ -22,53 +19,5 @@ class PdfPrint(private val printAttributes: PrintAttributes) {
                 )
             }
         }, null)
-    }
-
-    fun printToFile(adapter: PrintDocumentAdapter, file: File, onFinish: () -> Unit) {
-        adapter.onLayout(null, printAttributes, null, object : PrintDocumentAdapter.LayoutResultCallback() {
-            override fun onLayoutFinished(info: PrintDocumentInfo?, changed: Boolean) {
-                val pfd = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_WRITE)
-                adapter.onWrite(arrayOf(PageRange.ALL_PAGES), pfd, null, object : PrintDocumentAdapter.WriteResultCallback() {
-                    override fun onWriteFinished(pages: Array<PageRange>) {
-                        onFinish()
-                    }
-                })
-            }
-        }, null)
-    }
-
-    fun print(printAdapter: PrintDocumentAdapter, path: File, fileName: String) {
-        printAdapter.onLayout(null, printAttributes, null, object : PrintDocumentAdapter.LayoutResultCallback() {
-            override fun onLayoutFinished(info: PrintDocumentInfo?, changed: Boolean) {
-                printAdapter.onWrite(
-                    arrayOf(PageRange.ALL_PAGES),
-                    getOutputFile(path, fileName),
-                    CancellationSignal(),
-                    object : PrintDocumentAdapter.WriteResultCallback() {
-                        override fun onWriteFinished(pages: Array<out PageRange>?) {
-                            super.onWriteFinished(pages)
-                        }
-                    }
-                )
-            }
-        }, null)
-    }
-
-    private fun getOutputFile(path: File, fileName: String): ParcelFileDescriptor? {
-        if (!path.exists()) {
-            path.mkdirs()
-        }
-        val file = File(path, fileName)
-        return try {
-            file.createNewFile()
-            ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_WRITE)
-        } catch (e: Exception) {
-            Log.e(TAG, "Failed to open ParcelFileDescriptor", e)
-            null
-        }
-    }
-
-    companion object {
-        private val TAG = PdfPrint::class.java.simpleName
     }
 }
